e770c59c6137de67ba40f4ddbc232d8ef993d4aa
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2022-09-25 01:46:35 -0300

    teste

--- src-pwa/register-service-worker.js
+++ src-pwa/register-service-worker.js
@@ -14,23 +14,28 @@ register(process.env.SERVICE_WORKER_FILE, {
   // registrationOptions: { scope: './' },
 
   ready(/* registration */) {
-    // console.log('Service worker is active.')
+    console.log("Service worker is active.");
   },
 
   registered(/* registration */) {
-    // console.log('Service worker has been registered.')
+    console.log("Service worker has been registered.");
   },
 
   cached(/* registration */) {
-    // console.log('Content has been cached for offline use.')
+    console.log("Content has been cached for offline use.");
   },
 
   updatefound(/* registration */) {
-    // console.log('New content is downloading.')
+    console.log("New content is downloading.");
+    Notify.create({
+      message: "Realizando download de nova versao",
+      icon: "mdi-cloud-download-outline",
+      timeout: 10000,
+    });
   },
 
   updated(/* registration */) {
-    // console.log('New content is available; please refresh.')
+    console.log("New content is available; please refresh.");
     Notify.create({
       message: "Nova atualizaçao disponivel",
       icon: "mdi-cloud-download-outline",
@@ -43,10 +48,12 @@ register(process.env.SERVICE_WORKER_FILE, {
   },
 
   offline() {
-    // console.log('No internet connection found. App is running in offline mode.')
+    console.log(
+      "No internet connection found. App is running in offline mode."
+    );
   },
 
   error(/* err */) {
-    // console.error('Error during service worker registration:', err)
+    console.error("Error during service worker registration:", err);
   },
 });
