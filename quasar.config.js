c1f083f1804d28f1b8406eb1d828af63779f61fc
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2022-09-25 00:18:19 -0300

    teste

--- quasar.config.js
+++ quasar.config.js
@@ -141,13 +141,16 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli/developing-pwa/configuring-pwa
     pwa: {
       workboxMode: "generateSW", // or 'injectManifest'
-      workboxOptions: { skipWaiting: true, clientsClaim: true },
+      // workboxOptions: { skipWaiting: true, clientsClaim: true },
       injectPwaMetaTags: true,
       swFilename: "sw.js",
       manifestFilename: "manifest.json",
       useCredentialsForManifestTag: false,
       // useFilenameHashes: true,
-      // extendGenerateSWOptions (cfg) {}
+      extendGenerateSWOptions(cfg) {
+        cfg.skipWaiting = false;
+        cfg.clientsClaim = false;
+      },
       // extendInjectManifestOptions (cfg) {},
       // extendManifestJson (json) {}
       // extendPWACustomSWConf (esbuildConf) {}
